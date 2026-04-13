bdc3d29476c642624af9b5d015caabfd1934a074
Aevum <aevum@decess.us>
2026-04-12 20:00:01 -0400

    Split Discord sync into separate role list and member sync tasks

--- ffdiscord/tasks.py
+++ ffdiscord/tasks.py
@@ -5,18 +5,45 @@ from celery import shared_task
 from django.conf import settings
 
 from ffbot.utils import get_or_register_user
-from ffdiscord.utils import sync_user_roles
+from ffdiscord.utils import sync_guild_roles, sync_user_roles
 
 log = logging.getLogger(__name__)
 
 
 @shared_task
-def sync_all_guild_roles():
+def sync_discord_roles():
+    """Fetch all guild roles from Discord and upsert DiscordRole records."""
+    asyncio.run(_sync_roles())
+
+
+@shared_task
+def sync_all_guild_members():
     """Fetch all guild members from Discord, register any new users, and sync roles."""
-    asyncio.run(_sync_all())
+    asyncio.run(_sync_members())
 
 
-async def _sync_all():
+async def _sync_roles():
+    import discord
+    intents = discord.Intents.default()
+    bot = discord.Bot(intents=intents)
+
+    @bot.event
+    async def on_ready():
+        try:
+            guild = bot.get_guild(int(settings.DISCORD_REQUIRED_GUILD_ID))
+            if guild is None:
+                log.error("Guild %s not found", settings.DISCORD_REQUIRED_GUILD_ID)
+                return
+            all_roles = [(str(r.id), r.name) for r in guild.roles if not r.is_default()]
+            sync_guild_roles(all_roles)
+            log.info("Synced %d guild roles", len(all_roles))
+        finally:
+            await bot.close()
+
+    await bot.start(settings.DISCORD_BOT_TOKEN)
+
+
+async def _sync_members():
     import discord
     intents = discord.Intents.default()
     intents.members = True
