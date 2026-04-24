a7e4998ca0244f59259c2acd2db75f2e0819da86
Aevum <aevum@decess.us>
2026-04-24 14:51:32 -0400

    Seed permission groups in AppConfig.ready() - runs once on web container startup after migrations complete

--- ffsite/apps.py
+++ ffsite/apps.py
@@ -5,6 +5,5 @@ class FfsiteConfig(AppConfig):
     name = 'ffsite'
 
     def ready(self):
-        from django.db.models.signals import post_migrate
         from fforg.permissions import seed_permission_groups
-        post_migrate.connect(seed_permission_groups)
+        seed_permission_groups()
