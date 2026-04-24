90bf15906ee0806b0fd49a6659cc40d8995613e6
Aevum <aevum@decess.us>
2026-04-24 15:03:45 -0400

    Seed permission groups only on gunicorn startup - not during management commands or build

--- ffsite/apps.py
+++ ffsite/apps.py
@@ -1,13 +1,18 @@
+import sys
+
 from django.apps import AppConfig
+
+
+def _is_server_process():
+    """Return True only when running as a gunicorn web server process."""
+    return sys.argv[0].endswith('gunicorn')
 
 
 class FfsiteConfig(AppConfig):
     name = 'ffsite'
 
     def ready(self):
-        import sys
-        skip_commands = {'test', 'collectstatic', 'migrate', 'makemigrations', 'check'}
-        if any(cmd in sys.argv for cmd in skip_commands):
+        if not _is_server_process():
             return
         from fforg.permissions import seed_permission_groups
         seed_permission_groups()
