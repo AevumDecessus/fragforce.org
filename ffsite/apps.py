77411aad97a3069beed60dec0b8d2a2559b36fab
Aevum <aevum@decess.us>
2026-04-24 14:57:02 -0400

    Skip permission seeding during test runs - tests call seed_permission_groups() directly

--- ffsite/apps.py
+++ ffsite/apps.py
@@ -5,5 +5,8 @@ class FfsiteConfig(AppConfig):
     name = 'ffsite'
 
     def ready(self):
+        import sys
+        if 'test' in sys.argv:
+            return
         from fforg.permissions import seed_permission_groups
         seed_permission_groups()
