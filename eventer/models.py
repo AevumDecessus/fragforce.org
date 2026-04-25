efdb7f3b126a30e599b29d9df33f381d46d2c21e
Aevum <aevum@decess.us>
2026-04-24 20:10:33 -0400

    Add color field to EventRole with #417690 default

--- eventer/models.py
+++ eventer/models.py
@@ -74,6 +74,8 @@ class EventRole(models.Model):
     name = models.CharField(max_length=255, unique=True, db_index=True, null=False, blank=False)
     slug = models.SlugField(max_length=255, null=False, blank=False, db_index=True, unique=True)
     description = models.TextField(default='', blank=False, null=False)
+    color = models.CharField(max_length=7, default='#417690', blank=False, null=False,
+                             help_text="Hex color code for UI display (e.g. #417690)")
 
     def __str__(self):
         return self.name
